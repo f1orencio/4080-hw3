425a1f03da5847e7a4b1431a6b2e6ef69f0badec
Elvin Florencio <florencioged@icloud.com>
2026-09-17 23:04:19 -0700

    Complete Chapter 6

---
+++ com/craftinginterpreters/lox/TokenType.java
@@ -0,0 +1,32 @@
+package com.craftinginterpreters.lox;
+
+enum TokenType {
+  // Single-character tokens.
+  LEFT_PAREN, RIGHT_PAREN,
+  LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS,
+  SEMICOLON, SLASH, STAR,
+  QUESTION, COLON,
+
+  // One or two character tokens.
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+  LESS, LESS_EQUAL,
+  PLUS_PLUS, MINUS_MINUS,
+
+  // Literals.
+  IDENTIFIER, STRING, NUMBER,
+
+  // Keywords.
+  AND, OR,
+  TRUE, FALSE,
+  NIL,
+  VAR,
+  PRINT,
+  IF, ELSE,
+  WHILE,
+  FOR,
+
+  EOF
+}
